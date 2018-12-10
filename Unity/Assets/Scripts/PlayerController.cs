957fca5f3b91525f6626c1a447f90a7c2f7d6414
ThomasKozsir <kozsir515@googlemail.com>
2018-12-10 21:48:36 +0100

    added unity project with imported lab, some textures are broken though

---
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+
+        //Blickrichtung horizontal
+        transform.Rotate(0, x, 0);
+
+        //Bewegen
+        transform.Translate(0, 0, z);
+
+        // Generate a ray from the cursor position
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
+
+}
