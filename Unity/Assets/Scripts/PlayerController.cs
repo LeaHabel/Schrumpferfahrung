b6be7808e57e66249b3aa6e4df2b19727d09fda2
ThomasKozsir <kozsir515@googlemail.com>
2018-12-29 14:41:09 +0100

    added collision boxes for room, tables, glass box, lamps, window, shrunkbeam, energy source

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,15 @@
 
 public class PlayerController : MonoBehaviour
 {
+    string size;
+    bool isShrunk;
+
+    void Start(){
+        
+    }
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+       var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
 
@@ -15,7 +21,7 @@ public class PlayerController : MonoBehaviour
         transform.Translate(0, 0, z);
 
         // Generate a ray from the cursor position
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
     }
 
 }
